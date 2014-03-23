91f492da3c5e936f3c9db5e8f5396b7157061794
Aubrey Howell <simplyaubs@gmail.com>
2014-03-23 01:42:37 -0600

    Refactor Bob.rb cut out a ton of 'ifs'

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -3,15 +3,27 @@ class Bob
     case
       when shouting?(words)
         "Woah, chill out!"
-      when words.end_with?("?")
+      when question?(words)
         "Sure."
+      when silence?(words)
+        "Fine. Be that way!"
       else
         "Whatever."
     end
   end
 
+
+  private
   def shouting?(words)
     words == words.upcase && words.match(/[A-Z]/)
+  end
+
+  def question?(words)
+    words.end_with?("?")
+  end
+
+  def silence?(words)
+    words.strip.empty?
   end
 
 end
