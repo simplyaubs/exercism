9cee8a0d18053f0185bb65319a280a585527098a
Aubrey Howell <simplyaubs@gmail.com>
2014-03-23 23:49:30 -0600

    Worked through grains again...not done

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,15 +1,21 @@
 class Grains
-
   def square(num)
-    2 ** (num - 1)
+    2 ** (num -1)
   end
 
   def total
     total_grains = 0
-    (1..64).each do |i|
-      total_grains += square(i)
+    (1..64).each do |x|
+      total_grains += square(x)
     end
     total_grains
   end
-
 end
+
+#current_square = num
+#previous_square = num - 1
+#latest_total = previous_square * 2
+
+#(1..64).each do |num|
+#  num * 2 += num
+#end
